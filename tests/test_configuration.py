e85260ee094308d37f7e9d8bd3674f5bb092c654
Mattia D'Antonio <m.dantonio@cineca.it>
2017-07-19 17:06:31 +0200

    Testing configuration reader and basic converter

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,3 +1,52 @@
 
+import pytest
 from utilities.configuration import read
-from utilities.configuration import mix
+from utilities.basher import BashCommands
+
+
+def test():
+
+    project_conf = "projects/template/project_configuration.yaml" 
+    bash = BashCommands()
+    bash.create_directory("projects")
+    bash.create_directory("projects/template")
+    params = [
+        "utilities/projects_defaults.yaml",
+        project_conf
+    ]
+    bash.execute_command("cp", params)
+
+    try:
+        read("template")
+    except SystemExit:
+        pass
+    else:
+        pytest.fail("A SystemExit should be raised due to use of default conf")
+
+    params = [
+        "-i",
+        "s/My project/My custom title/g",
+        project_conf
+    ]
+    bash.execute_command("sed", params)
+
+    params = [
+        "-i",
+        "s/name: rapydo/name: myname/g",
+        project_conf
+    ]
+    bash.execute_command("sed", params)
+
+    params = [
+        "-i",
+        "s/Title of my project/My custom title/g",
+        project_conf
+    ]
+    bash.execute_command("sed", params)
+    conf = read("template")
+
+    assert "project" in conf
+    assert "name" in conf["project"]
+    assert conf["project"]["name"] == "myname"
+
+    bash.remove_directory("projects")
