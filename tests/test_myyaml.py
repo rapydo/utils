b863454ebaaa99f5e14ba7eb2c38bd17b3d5ed3d
Mattia D'Antonio <m.dantonio@cineca.it>
2017-07-19 16:18:45 +0200

    Fix path

--- tests/test_myyaml.py
+++ tests/test_myyaml.py
@@ -3,7 +3,7 @@ from utilities.myyaml import load_yaml_file
 
 
 def test():
-    yaml_path = "../utilities/projects_defaults.yaml"
+    yaml_path = "utilities/projects_defaults.yaml"
     load_yaml_file(yaml_path)
     yaml = load_yaml_file(yaml_path, keep_order=True)
 
