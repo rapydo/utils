71c690b85178538df3f7e5a92a6337a2787012ab
Mattia D'Antonio <m.dantonio@cineca.it>
2017-09-22 13:28:27 +0200

    Utilities from time conversion

--- utilities/time.py
+++ utilities/time.py
@@ -2,6 +2,11 @@
 
 import pytz
 from datetime import datetime
+import dateutil.parser
+
+from utilities.logs import get_logger
+
+log = get_logger(__name__)
 
 
 def timestamp_from_string(timestamp_string):
@@ -17,3 +22,27 @@ def timestamp_from_string(timestamp_string):
     aware_utc_dt = utc_dt.replace(tzinfo=pytz.utc)
 
     return aware_utc_dt
+
+
+def date_from_string(date, format="%d/%m/%Y"):
+
+    if date == "":
+        return ""
+    # datetime.now(pytz.utc)
+    try:
+        return_date = datetime.strptime(date, format)
+    except BaseException:
+        return_date = dateutil.parser.parse(date)
+
+    return pytz.utc.localize(return_date)
+
+
+def string_from_timestamp(timestamp):
+    if timestamp == "":
+        return ""
+    try:
+        date = datetime.fromtimestamp(float(timestamp))
+        return date.isoformat()
+    except BaseException:
+        log.warning("Errors parsing %s" % timestamp)
+        return ""
