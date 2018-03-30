db158ec3a078aa23514d7c1d66d20ef9b2c7b7ca
Mattia <m.dantonio81@gmail.com>
2018-03-30 14:51:39 +0200

    Utility to retrieve current time from web

--- utilities/time.py
+++ utilities/time.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pytz
+import urllib3
 from datetime import datetime
 import dateutil.parser
 
@@ -50,3 +51,11 @@ def string_from_timestamp(timestamp):
     except BaseException:
         log.warning("Errors parsing %s", timestamp)
         return ""
+
+
+def get_online_utc_time():
+    http = urllib3.PoolManager()
+    response = http.request('GET', "http://just-the-time.appspot.com/")
+
+    internet_time = response.data.decode('utf-8')
+    return datetime.strptime(internet_time.strip(), "%Y-%m-%d %H:%M:%S")
