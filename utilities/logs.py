a8970fb62c970a5ea438a7bd589a180a5a90da9e
Mattia <m.dantonio81@gmail.com>
2018-08-05 08:33:38 +0200

    Debugging errors on travis

--- utilities/logs.py
+++ utilities/logs.py
@@ -365,7 +365,6 @@ def set_global_log_level(package=None, app_level=None):
     logging.getLogger().setLevel(app_level)
     package_base = package.split('.')[0]
 
-    extra_packages = []
     for key, value in logging.Logger.manager.loggerDict.items():
 
         if not isinstance(value, logging.Logger):
@@ -389,13 +388,8 @@ def set_global_log_level(package=None, app_level=None):
         elif key in internal_packages:
             # print("internal", key, package)
             value.setLevel(app_level)
-        else:
-            extra_packages.append(key)
-
         # else:
         #     value.setLevel(external_level)
-
-        raise SystemExit(extra_packages)
 
 
 please_logme = LogMe()
