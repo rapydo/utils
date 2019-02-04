1b4439e99ebb08098e4b834c3d8d2e453b0b104e
Mattia <m.dantonio81@gmail.com>
2019-02-04 07:47:24 +0100

    Simplification of configuration.read function

--- utilities/configuration.py
+++ utilities/configuration.py
@@ -1,24 +1,32 @@
 # -*- coding: utf-8 -*-
 
-# import os
 from utilities import PROJECT_CONF_FILENAME, PROJECTS_DEFAULTS_FILE
 from utilities.myyaml import load_yaml_file
 from utilities.logs import get_logger
 
 log = get_logger(__name__)
 
-# SCRIPT_PATH = helpers.script_abspath(__file__)
 
+def load_project_configuration(path, file=None, do_exit=True):
 
-def load_project_configuration(project_path):
+    if file is None:
+        file = PROJECT_CONF_FILENAME
+
     args = {
-        'path': project_path,
+        'path': path,
         'skip_error': False,
         'logger': False,
-        'file': PROJECT_CONF_FILENAME,
+        'file': file,
         'keep_order': True
     }
-    return load_yaml_file(**args)
+    try:
+        log.checked("Found '%s/%s' configuration", path, file)
+        return load_yaml_file(**args)
+    except AttributeError as e:
+        if do_exit:
+            log.exit(e)
+        else:
+            raise AttributeError(e)
 
 
 def read(base_path, project_path=None, is_template=False, do_exit=True):
@@ -26,53 +34,21 @@ def read(base_path, project_path=None, is_template=False, do_exit=True):
     Read default configuration
     """
 
-    project_configuration_files = [
-        # DEFAULT
-        {
-            # 'path': SCRIPT_PATH,
-            'path': base_path,
-            'skip_error': False,
-            'logger': False,
-            'file': PROJECTS_DEFAULTS_FILE
-        }
-    ]
+    base_configuration = load_project_configuration(
+        base_path, file=PROJECTS_DEFAULTS_FILE, do_exit=do_exit)
 
-    if project_path is not None:
-        project_configuration_files.append(
-            # CUSTOM FROM THE USER
-            {
-                'path': project_path,
-                'skip_error': False,
-                'logger': False,
-                'file': PROJECT_CONF_FILENAME
-            }
-        )
-
-    confs = {}
-
-    for args in project_configuration_files:
-        try:
-            args['keep_order'] = True
-            f = args['file']
-            confs[f] = load_yaml_file(**args)
-            log.checked("Found '%s' rapydo configuration" % f)
-        except AttributeError as e:
-            if do_exit:
-                log.exit(e)
-            else:
-                raise AttributeError(e)
-
-    # Recover the two options
-    base_configuration = confs.get(PROJECTS_DEFAULTS_FILE)
     if project_path is None:
         return base_configuration
-    custom_configuration = confs.get(PROJECT_CONF_FILENAME, {})
+
+    custom_configuration = load_project_configuration(
+        project_path, file=PROJECT_CONF_FILENAME, do_exit=do_exit)
 
     # Verify custom project configuration
     prj = custom_configuration.get('project')
     if prj is None:
         raise AttributeError("Missing project configuration")
-    elif not is_template:
+
+    if not is_template:
 
         # Check if these three variables were changed from the initial template
         checks = {
@@ -83,15 +59,9 @@ def read(base_path, project_path=None, is_template=False, do_exit=True):
         for key, value in checks.items():
             if prj.get(key, '') == value:
 
-                # get file name with the load file utility
-                args = {}
-                kwargs = project_configuration_files.pop()
-                filepath = load_yaml_file(
-                    *args, return_path=True, **kwargs)
-
                 log.critical_exit(
-                    "\n\nYour project is not yet configured:\n" +
-                    "Please edit key '%s' in file %s" % (key, filepath)
+                    "Project not configured, mising key '%s' in file %s/%s.yaml",
+                    key, project_path, PROJECT_CONF_FILENAME
                 )
 
     # Mix default and custom configuration
