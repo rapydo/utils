80aadcae30d0a30f685a3fdbea4c741c056d9017
Mattia <m.dantonio81@gmail.com>
2019-07-16 13:40:32 +0200

    Fix tests for new configuration.read function

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import pytest
+from glom import glom
 from utilities.configuration import read
 from utilities import helpers
 from utilities import PROJECT_DIR
@@ -38,17 +39,17 @@ def test():
     # project_configuration is ok
     conf = read(DEFAULTS_PATH, project_file_path, PROJECT_DIR, SUBMODULES_DIR)
 
-    assert "project" in conf
-    assert "description" in conf["project"]
-    assert conf["project"]["description"] == "My description"
+    assert glom(conf, "project") is not None
+    assert glom(conf, "project.description") is not None
+    assert glom(conf, "project.description") == "My description"
 
     # project_configuration is missing required info
     bash.replace_in_file("project:", "blabla:", project_conf)
     try:
-        conf = read("template")
+        conf = read(DEFAULTS_PATH, project_file_path, PROJECT_DIR, SUBMODULES_DIR)
     except AttributeError:
         pass
     else:
         pytest.fail("This call should fail and raise an AttributeError")
 
-    # bash.remove_directory("projects")
+    bash.remove_directory("projects")
