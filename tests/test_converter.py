76a4da895a76da381b1ef6e93f4a5f4391282ac9
Mattia <m.dantonio81@gmail.com>
2019-11-13 17:32:50 +0100

    Removed unused code

--- tests/test_converter.py
+++
@@ -1,9 +0,0 @@
-import os
-from utilities.converter import convert_markdown_file
-
-
-def test():
-    convert_markdown_file("README.md")
-
-    # TODO: how to verify if the converted file is right?
-    assert os.path.isfile("README")
